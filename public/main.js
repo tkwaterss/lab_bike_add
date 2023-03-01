9bb3f31e3530c26039ffeead62227b1481d1856f
Tobin Waters <tkwaterss@gmail.com>
2023-03-01 13:31:02 -0700

    small change

--- public/main.js
+++ public/main.js
@@ -3,3 +3,6 @@ let bike = document.querySelector('img')
 bike.addEventListener('click', (event) => {
     alert('CONGRADULATIONS YOU HAVE PURCHASED THIS BIKE FOR $10,000.00! EXPECT IT TO ARRIVE IN 30 YEARS! THANK YOU!!!')
 })
+bike.addEventListener('mouseover', (event) => {
+    alert('CLICK ME!')
+})
