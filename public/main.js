efa33ee92973cc35a73524db11586a9aff921042
Tobin Waters <tkwaterss@gmail.com>
2023-03-01 12:57:07 -0700

    project is functional and ready to be hosted

---
+++ public/main.js
@@ -0,0 +1,5 @@
+let bike = document.querySelector('img')
+
+bike.addEventListener('click', (event) => {
+    alert('CONGRADULATIONS YOU HAVE PURCHASED THIS BIKE FOR $10,000.00! EXPECT IT TO ARRIVE IN 30 YEARS! THANK YOU!!!')
+})
